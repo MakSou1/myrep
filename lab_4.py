253a9083ca4198520437ed37e271880f2e7f2e3a
MakSou1 <127955554+MakSou1@users.noreply.github.com>
2023-05-31 14:49:18 +0300

    Update lab_4.py

--- lab_4.py
+++ lab_4.py
@@ -15,14 +15,14 @@ while True:
     empty_lines = content.count('\n')
     lines_with_a = sum('a' in line for line in content)
     a_count = sum(line.count('a') for line in content)
-    lines_with_roads = sum('roads' in line for line in content)
+    lines_with_roads = sum('end' in line for line in content)
 
     print(f"\nFile: {file_path}")
     print(f"total lines: {total_lines}")
     print(f"empty lines: {empty_lines}")
     print(f"lines with \"a\": {lines_with_a}")
     print(f"\"a\" count: {a_count}")
-    print(f"lines with \"roads\": {lines_with_roads}")
+    print(f"lines with \"end\": {lines_with_end}")
 
     answer = input("Do you want to analyze another file? (y/n): ")
     if answer.lower() != 'y':
